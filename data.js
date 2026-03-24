4c97b429c9e102a9f7706e62e7449873c8ea1687
josefkoloros <josef.koloros@gmail.com>
2026-03-24 09:22:05 +0100

    Update comments and keys in preset database

--- data.js
+++ data.js
@@ -1,29 +1,37 @@
-// data.js - Databáze zpoždění a polarity podle L-Acoustics Preset Guide v28.0
-// Klíče jsou tvořeny jako "top-sub-režim" (např. "kara2-sb18-standard")
+// data.js - Databáze zpoždění a polarity (L-Acoustics Preset Guide v28.0)
 
 const presetDatabase = {
-    // --- Kara II + SB18 ---
-    "kara2-sb18-standard": { topDelay: "2.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
-    "kara2-sb18-c": { topDelay: "8.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
-    "kara2-sb18-cx": { topDelay: "6.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    // --- KARA II + SB18 ---
+    "kara2-sb18-60": { topDelay: "2.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-sb18-60_c": { topDelay: "8.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-sb18-60_cx": { topDelay: "6.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-sb18-100": { topDelay: "0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-sb18-100_c": { topDelay: "5.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-sb18-100_cx": { topDelay: "4.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
     
-    // --- Kara II + KS21 ---
-    "kara2-ks21-standard": { topDelay: "0.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
-    "kara2-ks21-c": { topDelay: "6.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
-    "kara2-ks21-cx": { topDelay: "5.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    // --- KARA II + KS21 ---
+    "kara2-ks21-60": { topDelay: "0.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-ks21-60_c": { topDelay: "6.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-ks21-60_cx": { topDelay: "5.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-ks21-100": { topDelay: "0 ms", topPol: "+", subDelay: "0.5 ms", subPol: "+" },
+    "kara2-ks21-100_c": { topDelay: "5.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-ks21-100_cx": { topDelay: "4.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
 
-    // --- Kara II + KS28 ---
-    "kara2-ks28-standard": { topDelay: "0 ms", topPol: "+", subDelay: "5.0 ms", subPol: "+" },
-    "kara2-ks28-c": { topDelay: "0.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
-    "kara2-ks28-cx": { topDelay: "4.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    // --- KARA II + KS28 ---
+    "kara2-ks28-60": { topDelay: "0 ms", topPol: "+", subDelay: "5.0 ms", subPol: "+" },
+    "kara2-ks28-60_c": { topDelay: "0.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-ks28-60_cx": { topDelay: "4.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-ks28-100": { topDelay: "0 ms", topPol: "+", subDelay: "1.0 ms", subPol: "+" },
+    "kara2-ks28-100_c": { topDelay: "4.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "kara2-ks28-100_cx": { topDelay: "7.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
 
-    // --- A15 + KS21 ---
-    "a15-ks21-standard": { topDelay: "0 ms", topPol: "+", subDelay: "2.3 ms", subPol: "+" },
-    "a15-ks21-c": { topDelay: "9.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
-    "a15-ks21-cx": { topDelay: "8.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    // --- A15 + KS21 (pouze 60Hz v manuálu) ---
+    "a15-ks21-60": { topDelay: "0 ms", topPol: "+", subDelay: "2.3 ms", subPol: "+" },
+    "a15-ks21-60_c": { topDelay: "9.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "a15-ks21-60_cx": { topDelay: "8.0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
 
-    // --- A10 + KS21 ---
-    "a10-ks21-standard": { topDelay: "0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
-    "a10-ks21-c": { topDelay: "5.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
-    "a10-ks21-cx": { topDelay: "0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" }
+    // --- A10 + KS21 (pouze 100Hz v manuálu) ---
+    "a10-ks21-100": { topDelay: "0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "a10-ks21-100_c": { topDelay: "5.5 ms", topPol: "+", subDelay: "0 ms", subPol: "+" },
+    "a10-ks21-100_cx": { topDelay: "0 ms", topPol: "+", subDelay: "0 ms", subPol: "+" }
 };
