ec12083b4c2a5f582d056cf9d3e99c51a4d21733
josefkoloros <josef.koloros@gmail.com>
2026-03-24 08:34:02 +0100

    Add preset database for speaker configurations
    Added a preset database for various speaker configurations including KARA II, A15, and A10 models with their respective delays and polarities.

--- data.js
+++ data.js
@@ -1,1 +1,28 @@
+// data.js - Databáze zpoždění a polarity (L-Acoustics Preset Guide v28.0)
 
+const presetDatabase = {
+    // --- Kara II + SB18 ---
+    "KARA II-SB18-standard": { topDelay: "2.5 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    "KARA II-SB18-c": { topDelay: "8 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    "KARA II-SB18-cx": { topDelay: "6.5 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    
+    // --- Kara II + KS21 ---
+    "KARA II-KS21-standard": { topDelay: "0.5 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    "KARA II-KS21-c": { topDelay: "6 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    "KARA II-KS21-cx": { topDelay: "5.5 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+
+    // --- Kara II + KS28 ---
+    "KARA II-KS28-standard": { topDelay: "0 ms", topPolarity: "+", subDelay: "5 ms", subPolarity: "+" },
+    "KARA II-KS28-c": { topDelay: "0.5 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    "KARA II-KS28-cx": { topDelay: "4.5 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+
+    // --- A15 + KS21 ---
+    "A15-KS21-standard": { topDelay: "0 ms", topPolarity: "+", subDelay: "2.3 ms", subPolarity: "+" },
+    "A15-KS21-c": { topDelay: "9 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    "A15-KS21-cx": { topDelay: "8 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+
+    // --- A10 + KS21 ---
+    "A10-KS21-standard": { topDelay: "0 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    "A10-KS21-c": { topDelay: "5.5 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" },
+    "A10-KS21-cx": { topDelay: "0 ms", topPolarity: "+", subDelay: "0 ms", subPolarity: "+" }
+};
